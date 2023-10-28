bb8ebd83e10d925d439d966b873cbd986d111fcd
Lixuan <1366070554@qq.com>
2023-10-28 17:29:13 +0800

    new logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TextileService } from './textile/textile.service';
 import { TextileModule } from './textile/textile.module';
+import { LoggingService } from './logging/logging.service';
+import { LoggingController } from './logging/logging.controller';
+import { LoggingModule } from './logging/logging.module';
+import { TextileController } from './textile/textile.controller';
 
 @Module({
-  imports: [TextileModule],
-  controllers: [AppController],
-  providers: [AppService, TextileService],
+  imports: [TextileModule, LoggingModule],
+  controllers: [AppController, LoggingController,TextileController],
+  providers: [AppService, TextileService, LoggingService],
 })
 export class AppModule {}
